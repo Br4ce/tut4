efe149123c42ab4295df840b102b7ce2e8852fbb
Pengfei Yang <workspace4brace@126.com>
2024-08-17 21:05:59 +0800

    Add subtraction test and implement subtract method

--- src/test/java/org/example/CalcTest.java
+++ src/test/java/org/example/CalcTest.java
@@ -4,11 +4,27 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
-  Calc c = new Calc();
 
-    @Test
-    void testAddition() {
-        assertEquals(4, c.add(2,2));
+  private final Calc c = new Calc();
+
+  @Test
+  public void testAddition() {
+    assertEquals(4, c.add(2, 2));
+  }
+
+  @Test
+  public void testSubtraction() {
+    assertEquals(2, c.subtract(4, 2));
+  }
+
+  public class Calc {
+
+    public int add(int a, int b) {
+      return a + b;
     }
 
+    public int subtract(int a, int b) {
+      return a - b; // a - b
+    }
+  }
 }
